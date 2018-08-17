a3dbc9b400696066c64a917cd21cf93d96f122f7
ashwinchalaka <ashwin.chalaka@gmail.com>
2018-08-16 20:43:55 -0700

    updated tags

--- apps/loginAndRegistration/models.py
+++ apps/loginAndRegistration/models.py
@@ -7,19 +7,30 @@ EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 class UserManager(models.Manager):
     def basic_validator(self, postData):
         errors = {}
+        inRegistration = False
         if 'first_name' in postData:
-            if len(postData['first_name']) < 2:
+            inRegistration = True
+        if inRegistration:
+            if len(postData['first_name'].strip()) < 2:
                 errors["first_name"] = "First name should be at least 2 characters"
-            if len(postData['last_name']) < 2:
+            if len(postData['last_name'].strip()) < 2:
                 errors["last_name"] = "Last name should be at least 2 characters"
-        if not EMAIL_REGEX.match(postData['email']):
-            errors['email'] = "Invalid email"
+            if not EMAIL_REGEX.match(postData['email'].strip().lower()):
+                errors['email'] = "Invalid email"
+            if len(postData['password'].strip()) < 8:
+                errors["password"] = "Password should be at least 8 characters"
+            if postData['conf_password'].strip() != postData['password'].strip():
+                errors['conf_password'] = "Passwords do not match"
+        else:
+            if not EMAIL_REGEX.match(postData['email'].strip().lower()) or len(postData['password'].strip()) < 8:
+                errors["password"] = "Invalid email or password"
         return errors
 
 class User(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
+    password = models.TextField()
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
     
--- apps/loginAndRegistration/views.py
+++ apps/loginAndRegistration/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.core.urlresolvers import reverse
 from .models import *
+import bcrypt
 
 def index(request):
     if 'loggedIn' in request.session:
@@ -9,42 +10,44 @@ def index(request):
     return render(request, 'loginAndRegistration/index.html')
 
 def processRegistration(request):
-    # pass the post data to the method we wrote and save the response in a variable called errors
     errors = User.objects.basic_validator(request.POST)
-    # check if the errors object has anything in itcopy
     if len(errors):
-        # if the errors object contains anything, loop through each key-value pair and make a flash message
         for key, value in errors.items():
-            messages.error(request, value)
-        # redirect the user back to the form to fix the errors
+            messages.error(request, value, extra_tags = key)
         return redirect(reverse('home'))
     else:
-        # if the errors object is empty, that means there were no errors!
+        tempHash = bcrypt.hashpw(request.POST['password'].strip().encode(), bcrypt.gensalt())
         currentEmail = request.POST['email']
         if not User.objects.filter(email = currentEmail):
-            tempUser = User.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], email=request.POST['email'])
+            tempUser = User.objects.create(first_name=request.POST['first_name'].strip(), last_name=request.POST['last_name'].strip(), email=request.POST['email'].strip().lower(), password=tempHash)
             messages.success(request, "Successfully registered")
-            # redirect to a success route
         elif User.objects.filter(email = currentEmail):
-            messages.success(request, "That email is already registered")
+            messages.error(request, "That email is already registered")
         return redirect(reverse('home'))
 
 def processLogin(request):
     errors = User.objects.basic_validator(request.POST)
     if len(errors):
         for key, value in errors.items():
-            messages.error(request, value)
+            messages.error(request, value, extra_tags = key)
         return redirect(reverse('home'))
     else:
-        currentEmail = request.POST['email']
-        currentUser = User.objects.filter(email = currentEmail)
-        if currentUser:
-            request.session['loggedIn'] = currentUser.values()[0]['first_name']
+        currentEmail = request.POST['email'].strip()
+        savedUser = User.objects.filter(email = currentEmail)
+        if savedUser:
+            passMatch = bcrypt.checkpw(request.POST['password'].strip().encode(), savedUser.values()[0]['password'].encode())
+        else:
+            messages.error(request, "That email is not registered")
+        if savedUser and passMatch:
+            request.session['loggedIn'] = savedUser.values()[0]['first_name']
             return redirect(reverse('loggedin'))
     return redirect(reverse('home'))
 
 def login(request):
-    return render(request, 'loginAndRegistration/loginSuccess.html')
+    if 'loggedIn' in request.session:
+        return render(request, 'wall/wall.html')
+    else:
+        return redirect(reverse('home'))
 
 def processLogout(request):
     request.session.clear()
